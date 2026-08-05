2177a53c5b326d5646df66a56a60280e2b0b0fb5
Alexander Poge <flekssando@mail.ru>
2026-08-05 19:56:41 +0300

    add test Config

---
+++ internal/config/config_test.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"testing"
+)
+
+func TestGetRequiredEnv(t *testing.T) {
+	t.Setenv("TEST_KEY", "test-value")
+
+	value, err := getRequiredEnv("TEST_KEY")
+	if err != nil {
+		t.Fatalf("failed to get required env: %v", err)
+	}
+
+	if value != "test-value" {
+		t.Fatalf("expected value to be 'test-value', got '%s'", value)
+	}
+}
+
+func TestGetRequiredEnv_Missing(t *testing.T) {
+ 
+	t.Setenv("TEST_MISSING", "")
+
+	value ,err := getRequiredEnv("TEST_MISSING")
+
+	if err == nil {
+		t.Fatalf("expected error to be non-nil, got nil")
+	}
+
+	if value != "" {
+		t.Fatalf("expected value to be empty, got '%s'", value)
+	}
+}
+
+func TestNewConfig(t *testing.T) {
+	t.Setenv("HH_TOKEN", "test-token")
+	t.Setenv("")
+   
+}
